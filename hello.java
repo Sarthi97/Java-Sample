5d0f7ae153c30e87316b3a24fb34a368d9220a27
Sarthi700 <sarthi961715@gmail.com>
2025-04-08 13:17:54 +0530

    msg

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class hello {
+    public static void main(String args[]) {
+        int a = 5;
+        float b = 5.f;
+        double c = 5.5;
+        char d = 'a';
+        boolean e = true;
+        String f = "Hello, World!";
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Hello, World!"); 
+    }
+}
